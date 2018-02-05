ab8649868a7d2131572f7f49103064539a481dfa
dauss75 <dauss75@gmail.com>
2018-02-05 13:45:48 -0600

    fill db

--- generate_motif/script/create_fimo_2017_07_27.sh
+++ generate_motif/script/create_fimo_2017_07_27.sh
@@ -3,76 +3,76 @@
 # For all 51 chromosome/database combos, copy the data to the fimo table
 
 # 1 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr1_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/1.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 2 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr2_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/2.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 3 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr3_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/3.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 4 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr4_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/4.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 5 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr5_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/5.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 6 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr6_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/6.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 7 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr7_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/7.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 8 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr8_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/8.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 9 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr9_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/9.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 10 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr10_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/10.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 11 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr11_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/11.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 12 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr12_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/12.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 13 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr13_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/13.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 14 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr14_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/14.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 15 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr15_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/15.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 16 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr16_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/16.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 17 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr17_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/17.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 18 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr18_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/18.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 19 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr19_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/19.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 20 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr20_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/20.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 21 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr21_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/21.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # 22 chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chr22_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/22.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # X chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chrX_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/X.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # Y chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chrY_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/Y.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
 
 # MT chromosome
-psql fimo -c "copy fimo_hg38 from './fimo_output/chrMT_fimo_output.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
+psql fimo -c "copy fimo_hg38 from './fimo_output/MT.fa.txt' delimiter E'\t' CSV header NULL as 'NULL';" &
